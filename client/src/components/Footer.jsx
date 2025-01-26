15de9d5a60496f29e38fced954d51461604204d8
cpesar <cpesar1@gmail.com>
2025-01-26 01:21:39 -0700

    bringing this back to life

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">
-            &copy; {currentYear} BirdHouse
-          </Col>
+          <Col className="text-center py-3">&copy; {currentYear} BIRDHOUSE</Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
